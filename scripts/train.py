bc81fc1c39ac2504ddff28ba93c0da658f2512d2
whoami <whoami@whoami.com>
2018-08-07 11:21:42 +0800

    update

--- scripts/train.py
+++ scripts/train.py
@@ -25,7 +25,7 @@ def main():
     parser.add_argument("--alpha", default=0.025,
                         help="Set the starting learning rate: 0.025 suggested for skip-gram;  0.05 suggested for CBOW")
     parser.add_argument("--dim-embedding", "-dm", help="set size of word vectors (dim of embedding)", default=128)
-    parser.add_argument("--iter", "-it", default=5, help="Run more training iterations")
+    parser.add_argument("--iter", "-it", default=5, help="Run more training iterations", type=int)
     parser.add_argument("--cbow", help="Use the continuous bag of words model: 0 ~ skip gram; 1 ~ CBOW", default=1)
     parser.add_argument("--window-size", "-ws", help="set max skip length between words", default=5)
     parser.add_argument("--frequency-threshold", "-ft",
@@ -38,7 +38,7 @@ def main():
     parser.add_argument("--min-count", default=3,
                         help="This will discard words that appears less than <MIN-COUNT> times")
     parser.add_argument("--output-clusters", "-oc",
-                        help="nuber of output clusters: 0 ~ output word vectors; 1 ~ nubmber k-means clusters", default=0, type=int)
+                        help="number of output clusters: 0 ~ output word vectors, NO kmeans; >0 ~ nubmber of clusters in k-means", default=0, type=int)
     parser.add_argument("--debug", help="Set the debug mode: 2=more info during training", default=2)
     parser.add_argument("--binary", "-b",
                         help="Save the resulting vectors in binary moded: 0 text; 1 binary; 2 both", default=2, type=int)
@@ -54,14 +54,14 @@ def main():
 
     args.save_vocab = args.text_data_file.replace(".txt", ".vocab")
     if args.output_clusters > 0:
-        args.output_file = args.text_data_file.replace(".txt", ".cluster.txt")
+        args.output_file = args.text_data_file.replace(".txt", "_{:04d}.cluster.txt".format(args.iter))
     else:
         if args.binary == 0:
-            args.output_file = args.text_data_file.replace(".txt", ".embedding.txt")
+            args.output_file = args.text_data_file.replace(".txt", "_{:04d}.embedding.txt".format(args.iter))
         elif args.binary == 1:
-            args.output_file = args.text_data_file.replace(".txt", ".embedding.bin")
+            args.output_file = args.text_data_file.replace(".txt", "_{:04d}.embedding.bin".format(args.iter))
         else:
-            args.output_file = args.text_data_file.replace(".txt", ".embedding.txt")
+            args.output_file = args.text_data_file.replace(".txt", "_{:04d}.embedding.txt".format(args.iter))
 
     cmd = "../bin/word2vec -train {} -output {} -size {} -window {}  -sample {}   -hs {}   -negative {}   -threads {}    -iter {}   -min-count {} -classes {}  -binary {}   -save-vocab {} -cbow {} --alpha {}".format(
         args.text_data_file, args.output_file, args.dim_embedding, args.window_size, args.frequency_threshold, args.use_hierarchical_softmax,
@@ -69,6 +69,7 @@ def main():
     )
     print(cmd)
     subprocess.run(shlex.split(cmd))
+    print("See {} for result".format(args.output_file))
 
 
 if __name__ == '__main__':
