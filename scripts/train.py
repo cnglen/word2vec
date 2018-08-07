16a878ce512d6031116f3903ad7c9c490625d3b3
Jacky Wang <cnglen@gmail.com>
2018-08-07 08:24:03 +0800

    update

--- scripts/train.py
+++ scripts/train.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""train the word2vec model"""
+"""train the word2vec model
+
+input_file: 以 space/tab/eod 作为分词边界
+"""
 
 import argparse
 
@@ -24,29 +27,32 @@ def main():
 
     parser = argparse.ArgumentParser(description='Train word2vec model.',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("--train", help="use text data from <TRAIN> to train the model")
-    parser.add_argument(
-        "--output", help="Use <OUTPUT> to save the resulting word vectors / word clusters", default="/tmp/output")
-    parser.add_argument("--size", help="set sie of word vectors", default=100)
-    parser.add_argument("--window", help="set max skip length between words", default=5)
-    parser.add_argument(
-        "--sample", help="Set threshold for occurrence of words. Those that appear with higher frequency in the training data will be randomly down-sampled, useful range is (0, 1e-5)", default=1e-3)
-    parser.add_argument("-hs", help="Use Hierarchical Softmax, 0  not used; 1 use", default=0)
-    parser.add_argument(
-        "--negative", help="Number of negative examples, common values are 3 - 10 (0=not used)", default=5)
+
+    parser.add_argument("--text-data-file", "-tdf",
+                        help="use text data from <TEXT_DATA> to train the model, assuming tab/space/EOL spaced tokens")
+    parser.add_argument("--output-file",  "-of",
+                        help="Use <OUTPUT> to save the resulting word vectors / word clusters", default="/tmp/output")
+    parser.add_argument("--dim-embedding", "-dm", help="set size of word vectors (dim of embedding)", default=100)
+    parser.add_argument("--iter", "-it", default=5, help="Run more training iterations")
+    parser.add_argument("--window-size", "-ws", help="set max skip length between words", default=5)
+    parser.add_argument("--frequency-threshold", "-ft",
+                        help="Set threshold for occurrence of words. Those that appear with higher frequency in the training data will be randomly down-sampled, useful range is (0, 1e-5)", default=1e-3)
+    parser.add_argument("--use-hierarchical-softmax", "-uhs",
+                        help="Use Hierarchical Softmax, 0  not used; 1 use", default=0)
+    parser.add_argument("--number-negative", "-nn",
+                        help="Number of negative examples, common values are 3 - 10 (0=not used)", default=5)
     parser.add_argument("--threads", default=12, help="Use <THEADS> threads")
-    parser.add_argument("--iter", default=5, help="Run more training iterations")
     parser.add_argument("--min-count", default=5,
                         help="This will discard words that appears less than <MIN-COUNT> times")
-    parser.add_argument(
-        "--classes", help="Output word classes rather than word vectors default number of classes is 0 (vectors are written)", default=0)
-
-    parser.add_argument("--debug", help="Set the debug mode(2=more info during training)", default=2)
-    parser.add_argument("--binary", help="Save the resulting vectors in binary moded: 0 (off)", default=0)
-    parser.add_argument("--save-vocab", help="the vocabulary willbe saved to <SAVE-VOCAB>", default="/tmp/vocab")
-    parser.add_argument(
-        "--read-vocab", help="The vocabulary will be read from <READ-VOCAB> , not constructed from the training data")
-    parser.add_argument("--cbow", help="Use the continuous bag of words model: 0~skip gram; 1 ~ CBOS", default=1)
+    parser.add_argument("--output-classes", "-oc",
+                        help="whether output word classes rather than word vectors:  0 ~ vectors; 1 ~ k-means classes", default=0)
+    parser.add_argument("--debug", help="Set the debug mode: 2=more info during training", default=2)
+    parser.add_argument("--binary", help="Save the resulting vectors in binary moded: 0 binary; 1 text", default=0)
+    parser.add_argument("--save-vocab", "-sv",
+                        help="the vocabulary will be saved to <SAVE-VOCAB>", default="/tmp/vocab")
+    parser.add_argument("--read-vocab", "-rv",
+                        help="The vocabulary will be read from <READ-VOCAB> , not constructed from the training data")
+    parser.add_argument("--cbow", help="Use the continuous bag of words model: 0~skip gram; 1 ~ CBOW", default=1)
     parser.add_argument("--alpha", default=None,
                         help="Set the starting learning rate, default is 0.025 for skip-gram and 0.05 for CBOW")
 
@@ -57,10 +63,10 @@ def main():
     else:
         args.alhpa = 0.025
 
-    cmd = "../bin/word2vec -train {} -output {} -size {} -window {}    -sample {}   -hs {}   -negative {}   -threads {}    -iter {}   -min-count {} --classes {}  -binary {}   -save-vocab {} -cbow {} ".format(
-        args.train, args.output, args.size, args.window, args.sample, args.hs, args.negative, args.threads, args.iter, args.min_count, args.classes, args.binary, args.save_vocab, args.cbow
+    cmd = "../bin/word2vec -train {} -output {} -size {} -window {}    -sample {}   -hs {}   -negative {}   -threads {}    -iter {}   -min-count {} --classes {}  -binary {}   -save-vocab {} -cbow {} --alpha {}".format(
+        args.text_data_file, args.output_file, args.dim_embedding, args.window_size, args.frequency_threshold, args.use_hierarchical_softmax,
+        args.number_negative, args.threads, args.iter, args.min_count, args.output_classes, args.binary, args.save_vocab, args.cbow, args.alpha
     )
-
     subprocess.run(shlex.split(cmd))
 
 
